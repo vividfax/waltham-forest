bb6b3f4455454828c5a142c2b2d93ed5175ea2e6
vividfax <vividfax@gmail.com>
2021-08-30 13:23:20 +0100

    Add assemblage images

---
+++ preload.js
@@ -0,0 +1,27 @@
+let crane;
+
+let layer = [];
+
+let index = [0, 0, 0, 0, 0, 0];
+
+function preload() {
+
+    crane = loadModel("crane.obj");
+
+    i1a = loadImage("images/1a.png");
+    i2a = loadImage("images/2a.png");
+    i2b = loadImage("images/2b.png");
+    i3a = loadImage("images/3a.png");
+    i4a = loadImage("images/4a.png");
+    i5a = loadImage("images/5a.png");
+    i5b = loadImage("images/5b.png");
+    i6a = loadImage("images/6a.png");
+    i6b = loadImage("images/6b.png");
+
+    layer[0] = [i1a];
+    layer[1] = [i2a, i2b];
+    layer[2] = [i3a];
+    layer[3] = [i4a];
+    layer[4] = [i5a, i5b];
+    layer[5] = [i6a, i6b];
+}
--- sketch.js
+++ sketch.js
@@ -1,35 +1,12 @@
-let crane;
-let morris0, morris1, morris3, morris4, morris5, morris6, morris7;
-let assemblage, busstop;
-
-let images;
-let index = 0;
-
-let two;
-
-function preload() {
-
-    crane = loadModel("crane.obj");
-    morris0 = loadImage("images/morris0.jpg");
-    morris1 = loadImage("images/morris1.jpg");
-    morris2 = loadImage("images/morris2.jpg");
-    morris3 = loadImage("images/morris3.jpg");
-    morris4 = loadImage("images/morris4.jpg");
-    morris5 = loadImage("images/morris5.jpg");
-    morris6 = loadImage("images/morris6.jpg");
-    morris7 = loadImage("images/morris7.jpg");
-
-    images = [morris0, morris1, morris3, morris4, morris5, morris6, morris7];
-
-    assemblage = loadImage("images/assemblage.png");
-    busstop = loadImage("images/busstop.png");
-}
+let two1, two2;
 
 function setup() {
 
     createCanvas(windowWidth, windowHeight, WEBGL);
-    two = createGraphics(windowWidth, windowWidth);
-    two.pixelDensity(1);
+    two1 = createGraphics(windowHeight, windowHeight);
+    two1.pixelDensity(1);
+    two2 = createGraphics(windowHeight, windowHeight);
+    two2.pixelDensity(1);
 
     draw2d();
 
@@ -45,42 +22,54 @@ function draw() {
 function keyPressed() {
 
     if (key === "q" || key === "Q") {
-        change();
+        change(0);
     } else if (key === "w" || key === "W") {
-        change();
+        change(1);
     } else if (key === "e" || key === "E") {
-        change();
+        change(2);
     } else if (key === "r" || key === "R") {
-        change();
+        change(3);
     } else if (key === "t" || key === "T") {
-        change();
+        change(4);
     } else if (key === "y" || key === "Y") {
-        change();
+        change(5);
     }
 }
 
 function controls() {
 
-    select("#btn1").mousePressed(change);
-    select("#btn2").mousePressed(change);
-    select("#btn3").mousePressed(change);
-    select("#btn4").mousePressed(change);
-    select("#btn5").mousePressed(change);
-    select("#btn6").mousePressed(change);
+    select("#btn1").mousePressed(() => change(0));
+    select("#btn2").mousePressed(() => change(1));
+    select("#btn3").mousePressed(() => change(2));
+    select("#btn4").mousePressed(() => change(3));
+    select("#btn5").mousePressed(() => change(4));
+    select("#btn6").mousePressed(() => change(5));
 }
 
-function change() {
+function change(n) {
 
-    index++;
+    index[n]++;
 
-    if (index == images.length) {
-        index = 0;
+    if (index[n] == layer[n].length) {
+        index[n] = 0;
     }
     draw2d();
 }
 
 function draw2d() {
 
+    two1.image(layer[1][index[1]], 0, 0, two1.width, two1.width);
+    two1.image(layer[2][index[2]], 0, 0, two1.width, two1.width);
+    two1.image(layer[3][index[3]], 0, 0, two1.width, two1.width);
+    two1.image(layer[4][index[4]], 0, 0, two1.width, two1.width);
+    two1.image(layer[5][index[5]], 0, 0, two1.width, two1.width);
+
+    two2.image(layer[1][index[1]], 0, 0, two2.width, two2.width);
+    two2.image(layer[2][index[2]], 0, 0, two2.width, two2.width);
+    two2.image(layer[3][index[3]], 0, 0, two2.width, two2.width);
+    two2.image(layer[4][index[4]], 0, 0, two2.width, two2.width);
+    two2.image(layer[5][index[5]], 0, 0, two2.width, two2.width);
+    two2.image(layer[0][index[0]], 0, 0, two2.width, two2.width);
 }
 
 function draw3d() {
@@ -104,8 +93,8 @@ function drawBackground() {
     push();
     angleMode(DEGREES);
     noStroke();
-    texture(images[index]);
-    translate(-height/2, -height/2, -500);
+    texture(two2);
+    translate(-height/2, -height/2, -height);
     plane(height, height);
     translate(height, 0, 0);
     rotateY(180)
@@ -132,7 +121,7 @@ function drawCrane(delay) {
     translate(100, 100, 0);
     scale(height * 0.25);
     noStroke();
-    texture(images[index]);
+    texture(two1);
     model(crane);
     pop();
 }
@@ -149,7 +138,7 @@ function drawSphere(delay, size, distance) {
     translate(distance, distance, 0);
     scale(height * 0.25);
     noStroke();
-    texture(images[index]);
+    texture(two1);
     sphere(size);
     pop();
 }
