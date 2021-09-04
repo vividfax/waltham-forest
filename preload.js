161f70992d9ad93b0e2c2368c320d99c6cce36ce
vividfax <vividfax@gmail.com>
2021-09-04 21:55:00 +0100

    Refactor

--- preload.js
+++ preload.js
@@ -4,7 +4,7 @@ let street;
 let pencil;
 let flicks = [];
 
-let layer = [];
+let layer = [[],[],[],[],[],[]];
 
 let index = [0, 0, 0, 0, 0, 0];
 
@@ -18,21 +18,13 @@ function preload() {
     for (let i = 0; i < 5; i++) {
         flicks[i] = new Audio("sounds/flick" + i + ".wav");
     }
-
-    i1a = loadImage("images/1a.png");
-    i2a = loadImage("images/2a.png");
-    i2b = loadImage("images/2b.png");
-    i3a = loadImage("images/3a.png");
-    i4a = loadImage("images/4a.png");
-    i5a = loadImage("images/5a.png");
-    i5b = loadImage("images/5b.png");
-    i6a = loadImage("images/6a.png");
-    i6b = loadImage("images/6b.png");
-
-    layer[0] = [i1a];
-    layer[1] = [i2a, i2b];
-    layer[2] = [i3a];
-    layer[3] = [i4a];
-    layer[4] = [i5a, i5b];
-    layer[5] = [i6a, i6b];
+    layer[0].push(loadImage("images/1a.png"));
+    layer[1].push(loadImage("images/2a.png"));
+    layer[1].push(loadImage("images/2b.png"));
+    layer[2].push(loadImage("images/3a.png"));
+    layer[3].push(loadImage("images/4a.png"));
+    layer[4].push(loadImage("images/5a.png"));
+    layer[4].push(loadImage("images/5b.png"));
+    layer[5].push(loadImage("images/6a.png"));
+    layer[5].push(loadImage("images/6b.png"));
 }
